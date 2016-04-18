36cfbe8d74f6828b21717b3567f65fa806fab2e4
aaron.wu <aaron.wu@.chinapnr.com>
2016-04-18 15:53:16 +0800

    刷新充值功能

--- application/views/user/user_info.php
+++ application/views/user/user_info.php
@@ -82,7 +82,7 @@ $this->load->helper('form');
                         <p>邮箱: <?php echo $user_email;  ?></p>
                         <p>套餐: <span class="label label-info"> <?php echo $plan; ?> </span> </p>
                         <p>剩余套餐流量: <?php echo $money; ?>元  </p>
-                        <p>充值邀请码：<input type="text" maxlength="20" class="form-control" id="charge_code" name="charge_code" required > <button type="submit"class="btn btn-info btn-sm">充值</button></p>
+                        <p>充值邀请码：<input type="text" maxlength="40" size = "40" class="form-control" id="charge_code" name="charge_code" required > <button type="submit"class="btn btn-info btn-sm">充值</button></p>
                     </div><!-- /.box -->
                 </div>
             </div>
