cae6d0f098d6a4d053fdf3ce8dd1e868bcb879ff
aaron.wu <aaron.wu@.chinapnr.com>
2016-04-18 18:58:41 +0800

    刷新充值功能

--- application/views/user/user_info.php
+++ application/views/user/user_info.php
@@ -33,18 +33,18 @@ $this->load->helper('form');
 //            };
         $(function() {  
             $('#rechargeForm').submit(function() {
-                $.ajax({  
+                $(this).ajaxSubmit({  
                     type:"post",  //提交方式  
-                    dataType:"json", //数据类型  
+                    dataType:"json", //数据类型   
+                    async: false,
                     success:function(data){ //提交成功的回调函数  
-//                    if (data.result == "success") {
-//                        alert('222222');
-//                        window.location.href = "<?php echo site_url('user'); ?>";
-//                        } 
-//                    else {
-//                        alert(data.result);
-//                        }  
-                        alert(data.result); 
+                    if (data.result == "success") {
+                        alert('222222');
+                        window.location.href = "<?php echo site_url('user'); ?>";
+                        } 
+                    else {
+                        alert(data.result);
+                        }  
                     }  
                }); 
                 return false;
