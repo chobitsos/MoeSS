0aebfd26ba89aa93623cf326dcb9019559c32e83
aaron.wu <aaron.wu@.chinapnr.com>
2016-05-12 22:30:49 +0800

    图片压缩

--- application/views/site_code.php
+++ application/views/site_code.php
@@ -16,31 +16,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url("static/css/bootstrap.min.css"); ?>" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url("static/css/jumbotron-narrow.css"); ?>" rel="stylesheet">
+<!--    <link href="<?php echo base_url("static/css/jumbotron-narrow.css"); ?>" rel="stylesheet">-->
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type="text/javascript" src="<?php echo base_url("static/js/jquery-2.1.1.js"); ?>"></script>
 </head>
-<body style="background:#fff;" height="100%" >
+<body >
  <?php $this->load->view('index_nav');?>
-<!--<div class="bannerWrap clear">
-	<div class="banner clear"></div>
-</div>-->
 <br>
-<div class="content">
+<div style="background:#fff;">
+<div class="content" >
     <center><h2 class="sub-header" >若无可用邀请码发布，请联系群主获取，QQ群:469702630</h2></center>
 </div><br>
 <div class="container">
     <div class="jumbotron">
-        <p class="lead">邀请码实时刷新</p>
+        <p align='center' valign='center'>邀请码实时刷新</p>
     </div>
 
-    <div class="row marketing">
+    <div class="row marketing" >
         <h2 class="sub-header">邀请码</h2>
-        <div class="table-responsive">
+        <div class="table-responsive" >
             <table class="table table-striped">
                 <thead>
                 <tr>
@@ -63,6 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
 </div> <!-- /container -->
+</div>
 <?php
     $this->load->view('index_footer');
     $this->load->view('ana') ;?>
