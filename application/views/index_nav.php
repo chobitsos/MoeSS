a86fc2193cea75110369a0b75831df278625ef65
aaron.wu <aaron.wu@.chinapnr.com>
2016-05-12 14:54:04 +0800

    修改下载链接

--- application/views/index_nav.php
+++ application/views/index_nav.php
@@ -8,17 +8,19 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<link type="text/css" href="<?php echo base_url("static/css/index.css"); ?>" rel="stylesheet" />
-<div class="headWrap clear">
-	<div class="head clear">
-    	<a class="logo l" href="#" target="_blank"><img src="<?php echo base_url('images/logo.png'); ?>" width=100% height=100% border="0" /></a>
-        <ul class="menu r">
-        	<li class="mar_r"><a class="on" href="<?php echo site_url()?>">主页</a></li>
-            <li class="mar_r"><a href="<?php echo site_url('siteIndex/view_code') ;?>">邀请码</a></li>
-            <li class="mar_r"><a href="<?php echo site_url('user'); ?>">用户中心</a></li>
-            <li class="mar_r"><a href="#">VPN套餐价</a></li>
-            <li class="mar_r"><a href="<?php echo site_url('user/login'); ?>">登录</a></li>
-            <li class="reg"><a href="<?php echo site_url('user/register') ;?>">注册</a></li>
-        </ul>
-    </div>
+<link type="text/css" href="<?php echo base_url("static/css/common.css"); ?>" rel="stylesheet" />
+
+<div class="header">
+<div class="con">
+    <span class="log"></span>
+    <span class="nav">
+    <a href="<?php echo site_url()?>" class="cc">首页</a>
+    <a href="<?php echo site_url('siteIndex/view_code');?>">邀请码</a>
+    <a href="<?php echo site_url('user'); ?>">用户中心</a>
+    <a href="javascript:;">VPN套餐价</a>
+    <a href="<?php echo site_url('user/login'); ?>" class="tou">登录</a>
+    <a href="<?php echo site_url('user/register') ;?>" class="btn">注册</a>
+   </span>
+    <i class="clear"></i>
+</div>
 </div>
--- application/views/site_index.php
+++ application/views/site_index.php
@@ -8,73 +8,54 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="<?php echo site_url('favicon.ico'); ?>">
-
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0" />
     <title><?php echo SITE_NAME; ?></title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url("static/css/bootstrap.min.css"); ?>" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?php echo base_url("static/css/jumbotron-narrow.css"); ?>" rel="stylesheet">
-	<link type="text/css" href="<?php echo base_url("static/css/index.css"); ?>" rel="stylesheet" />
-	
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url("static/css/common.css"); ?>" />
+    <script type="text/javascript" src="<?php echo base_url("static/js/jquery-2.1.1.js"); ?>"></script>
 </head>
 <body>
 <?php $this->load->view('index_nav');?>
 <!--Banner-->
-<div class="bannerWrap clear">
-	<div class="banner clear"></div>
-</div>
+<a class="focus" href="javascript:;"><img src="<?php echo base_url('images/pic1.jpg'); ?>" /></a>
 <!--Banner end-->
-<!--Content-->
-<div class="con clear">
-	<h1>多设备接入</h1>
-    <h2>Land of aw Free multi device access</h2>
-    <p><img src="<?php echo base_url('images/titline.png'); ?>" width="390" height="10" border="0"/></p>
-    <ul class="conlist">
-    	<li class="conlist1 l mar_r1">
-        	<a href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/shadowsocks-nightly-2.9.5.apk">
-            	<p></p>
-                <span>Android</span>
-                <i>Android客户端安装</i>
-            </a>
-        </li>
-        <li class="conlist2 l mar_r1">
-        	<a href="https://itunes.apple.com/us/app/shadowsocks/id665729974?mt=8">
-            	<p></p>
-                <span>iOS</span>
-                <i>iOS客户端安装</i>
-            </a>
-        </li>
-        <li class="conlist3 l mar_r1">
-        	<a href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/Shadowsocks-2.5.8.zip">
-            	<p></p>
-                <span>Windows</span>
-                <i>Windows用户推荐此客户端</i>
-            </a>
-        </li>
-        <li class="conlist4 r">
-        	<a href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/ShadowsocksX-2.6.3.dmg">
-            	<p></p>
-                <span>GoAgentX</span>
-                <i>Mac用户推荐此客户端</i>
-            </a>
-        </li>
-    </ul>
+<div class="content">
+<div class="con">
+    <p class="msgt1">多设备接入</p>
+    <p class="msgt2">Land of aw Free multi device access</p>
+    <p class="msgt3"><i></i></p>
+    <br /><br />
+    <a class="pro1" href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/shadowsocks-nightly-2.9.5.apk"></a>
+    <a class="pro2" href="https://itunes.apple.com/us/app/shadowsocks/id665729974?mt=8"></a>
+    <a class="pro3" href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/Shadowsocks-2.5.8.zip"></a>
+    <a class="pro4" href="http://ssserver.blob.core.chinacloudapi.cn/shadowsocks/ShadowsocksX-2.6.3.dmg"></a>
 </div>
+</div>
+
+<script type="text/javascript">
+var isMobile=false;
+var sUserAgent=navigator.userAgent.toLowerCase();
+var bIsIpad=sUserAgent.match(/ipad/i)=='ipad';
+var bIsIphone=sUserAgent.match(/iphone os/i)=='iphone os';
+var bIsMidp=sUserAgent.match(/midp/i)=='midp';
+var bIsUc7=sUserAgent.match(/rv:1.2.3.4/i)=='rv:1.2.3.4';
+var bIsUc=sUserAgent.match(/ucweb/i)=='web';
+var bIsCE=sUserAgent.match(/windows ce/i)=='windows ce';
+var bIsWM=sUserAgent.match(/windows mobile/i)=='windows mobile';
+if(bIsIpad||bIsIphone||bIsMidp||bIsUc7||bIsUc||bIsCE||bIsWM){isMobile=true;}
+$(document).ready(function(){
+	if(isMobile==true){
+	$("span.log").css({"width":"100%","height":"100px","border-bottom":"solid 1px #0f1215"});
+	$("span.nav").css({"width":"100%","text-align":"center","padding":"20px 0px","text-align":"center","border-top":"solid 1px #333f4a"}).find("a").css({"margin-lef":"auto","margin":"5px"});
+	$("a.tou").addClass("btn").before("<br /><br />");
+	$("a.focus").css({"height":"240px"}).html("");
+	$("p.msgt1").css({"font-size":"1.8em"});
+	$("p.msgt2").css({"font-size":"1.4em"});
+	}
+});
+</script>
 <!--Content end-->
 <!--Footer-->
 <?php
